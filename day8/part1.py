000d3529eb1254039903591f9eb5901019be605f
benjamin-grappe <31959228+benjamin-grappe@users.noreply.github.com>
2018-12-09 13:57:50 +0100

    Minor simplification.

--- day8/part1.py
+++ day8/part1.py
@@ -1,6 +1,6 @@
 def readNode(descr):
-	nNode = int(descr.pop(0))
-	nProp = int(descr.pop(0))
+	nNode = descr.pop(0)
+	nProp = descr.pop(0)
 	res  = sum( [readNode(descr) for i in range(nNode)] )
 	res += sum( [descr.pop(0)    for i in range(nProp)] )
 	return res
