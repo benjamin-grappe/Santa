18c0368c188274693cc5f67c188ff5cb165aa50b
benjamin-grappe <31959228+benjamin-grappe@users.noreply.github.com>
2018-12-09 14:30:11 +0100

    Comments
    Less compact version, with comments.

--- day8/part1.py
+++ day8/part1.py
@@ -1,8 +1,23 @@
 def readNode(descr):
-	nNode = descr.pop(0)
-	nProp = descr.pop(0)
-	res  = sum( [readNode(descr) for i in range(nNode)] )
-	res += sum( [descr.pop(0)    for i in range(nProp)] )
+	'''
+	Take a list of integer, with the first elements being the number of nodes and
+	the number of metadata to be read.
+	Caution : Descr will be shorten while the function reads the node (and its children).
+	Returns the sum of the metadata of the higher level node and its children.
+	'''
+	nNode = descr.pop(0) # number of child nodes
+	nMeta = descr.pop(0) # number of properties
+	res = 0
+
+	for i in range(nNode):
+		res += readNode(descr) # add the result of each child node
+
+	# At this step, no more children nodes are in the list.
+	# Next values will be the own node's metadata
+	for i in range(nMeta):
+		res += descr.pop(0)
+
 	return res
-with open("input.txt", 'r') as f:
+
+with open("defi.txt", 'r') as f:
 	print readNode( [int(e) for e in f.read().split(' ') if e != ""] )
